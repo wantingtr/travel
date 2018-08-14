ab6e2fe3052c2a69ac790ea6b7b01555b0c374a8
traveller <33363430+wantingtr@users.noreply.github.com>
2018-08-14 15:38:31 +0800

    project initialization

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
+    <!--显示的是当前路由地址所对应的内容 -->
   </div>
 </template>
 
@@ -13,11 +13,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 50px;
 }
 </style>
---
+++ src/pages/home/home.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+  <div class="">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style lang="css">
+
+</style>
