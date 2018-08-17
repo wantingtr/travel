5f327cb71eb2723c106f7b1c8955b31d95054ee6
wantingtr <wantingtr@163.com>
2018-08-17 21:15:48 +0800

    add the image swiper

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,21 @@
 <template lang="html">
   <div class="">
     <home-header></home-header>
+    <home-swiper></home-swiper>
+    <div class="">
+      test
+    </div>
   </div>
 </template>
 
 <script>
 import homeHeader from './components/header.vue'
+import homeSwiper from './components/swiper.vue'
 export default {
   name: 'home',
   components: {
-    homeHeader
+    homeHeader,
+    homeSwiper
   }
 }
 </script>
